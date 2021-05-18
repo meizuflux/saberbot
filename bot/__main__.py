3b10708c1c7b11b311ae17265ac5feb2dfde62bd
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-05-18 12:13:57 -0400

    messages to let you know you are connected

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,13 +4,7 @@ from pathlib import Path
 import logging
 
 WORKING_DIRECTORY = str(Path(__file__).parent.parent).replace("\\", "/") + "/"
-logger = logging.getLogger("bot")
-logging.basicConfig(
-    format="{asctime} {levelname:<7} {name:<15} |:| {message} |:| {pathname}:{lineno}",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    style="{",
-    level=logging.INFO
-)
+
 
 with open(WORKING_DIRECTORY + "config.toml") as f:
     config = toml.loads(f.read())
@@ -19,5 +13,4 @@ bot = Bot(command_prefix=config['core']['prefix'])
 bot.config = config
 
 if __name__ == "__main__":
-    logger.info("Launching Bot")
     bot.run(bot.config['core']['token'])

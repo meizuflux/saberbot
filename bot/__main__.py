b63aa34fa5cb7f6e80faf5aa0d1500333e9819ae
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-05-19 10:18:02 -0400

    comments, alternate methods for deploying

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,15 +3,17 @@ from bot import Bot
 from pathlib import Path
 import time
 
+# the working directory for the bot, kinda hacky tho
 WORKING_DIRECTORY = str(Path(__file__).parent.parent).replace("\\", "/") + "/"
 
-
+# load config files
 with open(WORKING_DIRECTORY + "config.toml") as f:
     config = toml.loads(f.read())
 
-time.sleep(10)
+# initialize the bot
 bot = Bot(command_prefix=config['core']['prefix'])
 bot.config = config
 
 if __name__ == "__main__":
+    # only run the bot if this file is being run directly
     bot.run(bot.config['core']['token'])
--- bot/bot.py
+++ bot/bot.py
@@ -3,23 +3,27 @@ import logging
 from discord.ext import commands
 from asyncpg import create_pool
 
+# setup logging so we know wtf is going on with the bot
 logger = logging.getLogger("bot")
 logging.basicConfig(
-    format="[{asctime}] {levelname:<7} {name}: {message}",
+    format="[{asctime}] {levelname:<7} {name}: {message}", # you can change formatting if you want
     datefmt="%Y-%m-%d %H:%M:%S",
     style="{",
-    level=logging.INFO
+    level=logging.INFO # change this to logging.DEBUG if something is going wrong during startup
 )
 
 class Bot(commands.Bot):
+    """Subclassed bot for added functionality and cleaner code"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.loop = asyncio.get_event_loop()
+        # create a database connection
         self.pool = self.loop.run_until_complete(
-            create_pool(port=5432, password='secret', database='bot', host='db', user='ppotatoo', loop=self.loop)
+            create_pool(port=5432, password='secret', database='bot', host='db', user='postgres', loop=self.loop)
         )
 
     async def on_ready(self):
+        """Lets us know when the bot is online. If you don't see this something went wrong"""
         message = (
             "Connected to Discord",
             "Username : {0}".format(self.user),

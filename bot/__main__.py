c57e5ac06f4470d7b0405cbe870786d05e52aae1
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-05-20 15:15:12 -0400

    readme + easier prefix config

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,13 @@
 from bot import Bot
+import argparse
+
+parser = argparse.ArgumentParser(allow_abbrev=False)
+parser.add_argument("-p", "--prefix", default="!")
+
+args = parser.parse_args()
 
 # initialize the bot
-bot = Bot(command_prefix="!")
+bot = Bot(command_prefix=args.prefix)
 
 if __name__ == "__main__":
     # only run the bot if this file is being run directly

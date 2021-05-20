8303731a13208e53db861b887ad945b20c2f6a2c
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-05-20 14:48:10 -0400

    remove unused import

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,18 +1,7 @@
-import toml
 from bot import Bot
-from pathlib import Path
-import time
-
-# the working directory for the bot, kinda hacky tho
-WORKING_DIRECTORY = str(Path(__file__).parent.parent).replace("\\", "/") + "/"
-
-# load config files
-with open(WORKING_DIRECTORY + "config.toml") as f:
-    config = toml.loads(f.read())
 
 # initialize the bot
-bot = Bot(command_prefix=config['core']['prefix'])
-bot.config = config
+bot = Bot(command_prefix="!")
 
 if __name__ == "__main__":
     # only run the bot if this file is being run directly

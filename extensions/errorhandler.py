2326eeb2f73d1b707f26dee1cfe5acb50f31e001
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-05-21 18:40:17 -0400

    forgot to commit, uh i moved stuff around, and I added a chart command

---
+++ extensions/errorhandler.py
@@ -0,0 +1,92 @@
+import traceback
+from contextlib import suppress
+
+import discord
+from discord.ext import commands
+
+from bot import Bot
+
+
+class Handler(commands.Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @staticmethod
+    async def handle_command_error(ctx: commands.Context, error: commands.CommandError):
+        owner_reinvoke = (
+            commands.MissingAnyRole,
+            commands.MissingPermissions,
+            commands.MissingRole,
+            commands.CommandOnCooldown,
+            commands.DisabledCommand,
+        )
+        if isinstance(error, owner_reinvoke):
+            await ctx.reinvoke()
+            return
+
+        command = None
+
+        if ctx.command:
+            if ctx.command.has_error_handler():
+                return
+
+            cog: commands.Cog = ctx.cog
+            if cog and cog.has_error_handler():
+                return
+
+            command = ctx.command.qualified_name
+
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, commands.CheckFailure):
+            return await ctx.send(str(error))
+
+        if isinstance(error, commands.NoPrivateMessage):
+            with suppress(discord.HTTPException):
+                await ctx.author.send(f"{command} can only be used within a server.")
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            errors = str(error).split(" ", maxsplit=1)
+            msg = (
+                f'`{errors[0]}` {errors[1]}\n'
+                f'You can view the help for this command with `{ctx.prefix}help` `{command}`'
+            )
+            return await ctx.send(msg)
+
+        if isinstance(error, commands.DisabledCommand):
+            return await ctx.send(f'`{command}` has been disabled.')
+
+        if isinstance(error, commands.BadArgument):
+            return await ctx.send(str(error))
+
+    async def handle_library_error(self, ctx: commands.Context, error: discord.DiscordException):
+        if isinstance(error, commands.CommandError):
+            await self.handle_command_error(ctx, error)
+        else:
+            await ctx.send(str(error))
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: Exception):
+        error = getattr(error, 'original', error)
+        if isinstance(error, discord.DiscordException):
+            await self.handle_library_error(ctx, error)
+        else:
+            if ctx.command:
+                if ctx.command.has_error_handler():
+                    return
+
+                cog: commands.Cog = ctx.cog
+                if cog and cog.has_error_handler():
+                    return
+
+
+            formatted = traceback.format_exception(type(error), error, error.__traceback__)
+
+            await ctx.send(
+                f"Oops, an error occurred. Sorry about that."
+                f"```py\n{''.join(formatted)}\n```"
+            )
+
+
+def setup(bot):
+    bot.add_cog(Handler(bot))
---
+++ extensions/utils/scoresaber.py
@@ -0,0 +1,37 @@
+import re
+from urllib.parse import quote
+
+from discord.ext import commands
+
+LINK_REGEX = re.compile(r"https?:\/\/(?:(?:new)\.)?scoresaber\.com\/u\/(?P<id>[0-9]{16,20})")
+API_URL = "https://new.scoresaber.com/api"
+
+
+async def scoresaber_id_from_query(ctx: commands.Context, query: str) -> dict:
+    query = quote(query.upper())
+    async with ctx.bot.session.get(API_URL + "/players/by-name/" + query) as resp:
+        data = await resp.json()
+    try:
+        return await get_profile(ctx, data['players'][0]['playerId'])
+    except KeyError:
+        raise commands.BadArgument(data['error']['message'])
+
+
+async def get_profile(ctx: commands.Context, scoresaber_id: int) -> dict:
+    url = API_URL + "/player/" + str(scoresaber_id) + "/full"
+    await ctx.trigger_typing()
+    async with ctx.bot.session.get(url) as resp:
+        data = await resp.json()
+    return data
+
+
+class ScoreSaberQueryConverter(commands.Converter):
+    async def convert(self, ctx: commands.Context, query: str) -> dict:
+        url_match = LINK_REGEX.match(query)
+        if len(query) < 4 or len(query) > 31 and not url_match:
+            raise commands.BadArgument("Please enter a query between 3 and 32 characters!")
+
+        if url_match:
+            return await get_profile(ctx, int(url_match['id']))
+
+        return await scoresaber_id_from_query(ctx, query)

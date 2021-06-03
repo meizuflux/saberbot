e8727a4a4e01e8e10dc25cce55d20c473ab87007
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-06-03 17:24:31 +0000

    experimental schema

--- schema.sql
+++ schema.sql
@@ -1,14 +1,30 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT PRIMARY KEY,
-    scoresaber_id VARCHAR UNIQUE,
-    rank BIGINT,
-    pp DOUBLE PRECISION,
-    change BIGINT,
-    play_count JSONB, /* {"total": 41234, "ranked": 5342} */
-    score JSONB, /* same thing as above */
-    average_accuracy DOUBLE PRECISION,
-    favorite_song JSONB DEFAULT '{"name": null, "url": null}'::jsonb, /* {"name": "Reality Check", "url": "url to song"} */
-    favorite_saber JSONB DEFAULT '{"name": null, "url": null}'::jsonb, /* same format as above */
-    headset VARCHAR,
+    snowflake BIGINT PRIMARY KEY,
+    id VARCHAR UNIQUE,
+    song JSONB DEFAULT '{"name": null, "url": null}'::jsonb, /* {"name": "Reality Check", "url": "url to song"} */
+    saber JSONB DEFAULT '{"name": null, "url": null}'::jsonb, /* same format as above */
+    hmd VARCHAR,
     grip VARCHAR
 );
+
+CREATE TABLE IF NOT EXISTS stats (
+    // playerInfo
+    id VARCHAR PRIMARY KEY REFERENCES users.id ON DELETE CASCADE,
+    name TEXT,
+    avatar VARCHAR,
+    COUNTRY CHAR(2)
+    rank BIGINT,
+    country_rank BIGINT,
+    player_role TEXT DEFAULT null,
+    history BIGINT[],
+
+    // scoreStats
+    avg_ranked_acc DOUBLE PRECISION, 
+    total_score BIGINT,
+    ranked_score BIGINT,
+    total_played INT,
+    ranked_played INT
+)
+
+CREATE INDEX scoresaber_id ON users (id)
+CREATE INDEX stats_id ON stats (id)
